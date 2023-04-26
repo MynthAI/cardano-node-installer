735442a9ce793f33636dc1b21d53e0209d3d622c
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2023-04-26 15:08:50 +0200

    Creating a new user and database for postgres

--- bin/install-postgres.sh
+++ bin/install-postgres.sh
@@ -31,7 +31,27 @@ sudo sed -i "s/#log_rotation_age = 1d/log_rotation_age = 1d/g" /etc/postgresql/*
 sudo sed -i "s/#log_rotation_size = 10MB/log_rotation_size = 10MB/g" /etc/postgresql/*/main/postgresql.conf
 
 # Allow connections from any IP address
-echo "host all all 0.0.0.0/0 md5" | sudo tee -a /etc/postgresql/*/main/pg_hba.conf
+echo "host all all 0.0.0.0/0 md5" | sudo tee -a /etc/postgresql/*/main/pg_hba.conf > /dev/null
 
 # Restart the Postgres service to apply the changes
 sudo systemctl restart postgresql
+
+# Set up a new Postgres database
+database="db_$(openssl rand -hex 4)"
+username="user_$(openssl rand -hex 4)"
+password="$(openssl rand -base64 30)"
+sudo -i -u postgres psql \
+    -c "CREATE USER ${username} WITH PASSWORD '${password}';"
+sudo -i -u postgres psql \
+    -c "CREATE DATABASE ${database} OWNER ${username};"
+sudo -i -u postgres psql \
+    -c "GRANT ALL PRIVILEGES ON DATABASE ${database}
+        TO ${username};"
+
+sudo mkdir -p /root/secrets
+echo -n "$database" | sudo tee /root/secrets/postgres_db \
+    > /dev/null
+echo -n "$username" | sudo tee /root/secrets/postgres_user \
+    > /dev/null
+echo -n "$password" | sudo tee /root/secrets/postgres_password \
+    > /dev/null
