08cb455bc762719157bf030329be92cad2b0aa04
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2023-04-26 15:33:38 +0200

    Small refactor

--- bin/install-postgres.sh
+++ bin/install-postgres.sh
@@ -13,13 +13,11 @@ POSTGRES_PASSWORD="$(openssl rand -base64 30)"
 
 # Create a .pgpass file to store the credentials
 PGPASS_FILE="/root/.pgpass"
-echo "localhost:*:*:postgres:${POSTGRES_PASSWORD}" | \
-    sudo tee "${PGPASS_FILE}" > /dev/null
-chmod 600 "${PGPASS_FILE}"
+sudo tee "${PGPASS_FILE}" <<< "localhost:*:*:postgres:${POSTGRES_PASSWORD}" > /dev/null
+sudo chmod 600 "${PGPASS_FILE}"
 
 # Set the password for the 'postgres' user
-sudo -i -u postgres psql \
-    -c "ALTER USER postgres WITH PASSWORD '${POSTGRES_PASSWORD}';"
+sudo -i -u postgres psql -c "ALTER USER postgres WITH PASSWORD '${POSTGRES_PASSWORD}';"
 
 # Configure Postgres for production use
 sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/*/main/postgresql.conf
@@ -40,18 +38,11 @@ sudo systemctl restart postgresql
 database="db_$(openssl rand -hex 4)"
 username="user_$(openssl rand -hex 4)"
 password="$(openssl rand -base64 30)"
-sudo -i -u postgres psql \
-    -c "CREATE USER ${username} WITH PASSWORD '${password}';"
-sudo -i -u postgres psql \
-    -c "CREATE DATABASE ${database} OWNER ${username};"
-sudo -i -u postgres psql \
-    -c "GRANT ALL PRIVILEGES ON DATABASE ${database}
-        TO ${username};"
+sudo -i -u postgres psql -c "CREATE USER ${username} WITH PASSWORD '${password}';"
+sudo -i -u postgres psql -c "CREATE DATABASE ${database} OWNER ${username};"
+sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE ${database} TO ${username};"
 
 sudo mkdir -p /root/secrets
-echo -n "$database" | sudo tee /root/secrets/postgres_db \
-    > /dev/null
-echo -n "$username" | sudo tee /root/secrets/postgres_user \
-    > /dev/null
-echo -n "$password" | sudo tee /root/secrets/postgres_password \
-    > /dev/null
+sudo tee /root/secrets/postgres_db <<< "$database" > /dev/null
+sudo tee /root/secrets/postgres_user <<< "$username" > /dev/null
+sudo tee /root/secrets/postgres_password <<< "$password" > /dev/null
